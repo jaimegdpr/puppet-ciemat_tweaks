ca8ef5d4f740c802fd1b7c98239b3188805107ae
andelpe <andelpe@ciemat.es>
2018-09-28 12:29:50 +0200

    Mimicking test env: Emptying 'ciemat_tweaks::folders' since we'll be now using 'ciemat_get_files' unmanaged list

--- manifests/folders.pp
+++ manifests/folders.pp
@@ -1,10 +1,10 @@
 class ciemat_tweaks::folders{
 
-    file { ['/root/packages', '/root/scripts', '/root/gmetric', '/root/nsca', '/root/tgz', '/root/install' ]:
-        ensure => directory,
-        mode => '0755',
-        owner => 'root',
-        group => 'root'
-     }
+#    file { ['/root/packages', '/root/scripts', '/root/gmetric', '/root/nsca', '/root/tgz', '/root/install' ]:
+#        ensure => directory,
+#        mode => '0755',
+#        owner => 'root',
+#        group => 'root'
+#     }
 
 }
